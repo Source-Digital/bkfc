43a5508ce7a82b71e5dfd5e00811965eb1feb13d
Kevin Source Digital <44618327+kevin-source-digital@users.noreply.github.com>
2023-11-29 16:26:15 -0500

    updates

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -358,55 +358,6 @@
     <div class="row q-px-md q-pt-md">
       <div class="col">
         <div class="row">
-          <div class="col text-h6">GET READY FOR BKFC THAILAND 5 NOV 4TH</div>
-        </div>
-      </div>
-    </div>
-    <div class="row no-wrap overflow-auto">
-      <q-card
-        dark
-        class="q-ma-md"
-        style="min-width: 400px"
-        v-for="item in BKFCTHAILANDFEED"
-        :key="item.id"
-      >
-        <q-img :src="item.media_group[0].media_item[0].src" />
-        <div class="row q-px-sm">
-          <q-expansion-item
-            switch-toggle-side
-            expand-separator
-            :label="item.title"
-          >
-            <div class="row">
-              <div class="col-auto text-bold">Title:</div>
-              <div class="col-auto q-pl-sm text-wrap">{{ item.title }}</div>
-            </div>
-            <div class="row items-center">
-              <div class="col-auto text-bold">VideoURL:</div>
-              <q-btn
-                dense
-                class="col-auto q-pl-sm"
-                @click="copyToClipboard(item.content.src)"
-                >Click to Copy URL</q-btn
-              >
-            </div>
-            <div class="row items-center">
-              <div class="col-auto text-bold">ImageURL</div>
-
-              <q-btn
-                dense
-                class="col-auto q-pl-sm"
-                @click="copyToClipboard(item.media_group[0].media_item[0].src)"
-                >Click to Copy URL</q-btn
-              >
-            </div>
-          </q-expansion-item>
-        </div>
-      </q-card>
-    </div>
-    <div class="row q-px-md q-pt-md">
-      <div class="col">
-        <div class="row">
           <div class="col text-h6">PROSPECTS: THE NEW GENERATION</div>
         </div>
       </div>
@@ -417,55 +368,6 @@
         class="q-ma-md"
         style="min-width: 400px"
         v-for="item in PROSPECTSFeed"
-        :key="item.id"
-      >
-        <q-img :src="item.media_group[0].media_item[0].src" />
-        <div class="row q-px-sm">
-          <q-expansion-item
-            switch-toggle-side
-            expand-separator
-            :label="item.title"
-          >
-            <div class="row">
-              <div class="col-auto text-bold">Title:</div>
-              <div class="col-auto q-pl-sm text-wrap">{{ item.title }}</div>
-            </div>
-            <div class="row items-center">
-              <div class="col-auto text-bold">VideoURL:</div>
-              <q-btn
-                dense
-                class="col-auto q-pl-sm"
-                @click="copyToClipboard(item.content.src)"
-                >Click to Copy URL</q-btn
-              >
-            </div>
-            <div class="row items-center">
-              <div class="col-auto text-bold">ImageURL</div>
-
-              <q-btn
-                dense
-                class="col-auto q-pl-sm"
-                @click="copyToClipboard(item.media_group[0].media_item[0].src)"
-                >Click to Copy URL</q-btn
-              >
-            </div>
-          </q-expansion-item>
-        </div>
-      </q-card>
-    </div>
-    <div class="row q-px-md q-pt-md">
-      <div class="col">
-        <div class="row">
-          <div class="col text-h6">KNUCKLE UP AND THROW DOWN!</div>
-        </div>
-      </div>
-    </div>
-    <div class="row no-wrap overflow-auto">
-      <q-card
-        dark
-        class="q-ma-md"
-        style="min-width: 400px"
-        v-for="item in knuckleUpThrowDownFeed"
         :key="item.id"
       >
         <q-img :src="item.media_group[0].media_item[0].src" />
